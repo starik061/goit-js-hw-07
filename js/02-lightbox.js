1212c91387e9cb3ee3c63bf74d2e3ef7ae897bde
starik061 <starik061@gmail.com>
2023-04-07 11:37:14 +0300

    Task #2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+createGallery(galleryItems);
+
+function createGallery(galleryData) {
+  gallery.innerHTML = galleryData
+    .map(
+      (galleryItem) => `<li class="gallery__item">
+  <a class="gallery__link" href="${galleryItem.original}">
+    <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+  </a>
+</li>`
+    )
+    .join("");
+}
+
+let galleryModalWindow = new SimpleLightbox(".gallery__link", {
+  captions: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
